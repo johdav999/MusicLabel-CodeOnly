8ba9c4b09076c30819089d65ddfce069b123ba28
johdav999 <johandavidsson@hotmail.se>
2025-08-26 19:00:34 +0200

    Use public list view API for entry widget class

--- Source/LabelManager/Private/UI/Widget_NewsFeed.cpp
+++ Source/LabelManager/Private/UI/Widget_NewsFeed.cpp
@@ -1,44 +1,41 @@
 #include "UI/Widget_NewsFeed.h"
-#include "UI/Widget_NewsItemEntry.h"
 #include "Components/ListView.h"
 #include "TimerManager.h"
+#include "UI/Widget_NewsItemEntry.h"
 
-void UWidget_NewsFeed::NativeConstruct()
-{
-    Super::NativeConstruct();
-    if (NewsList && !NewsList->EntryWidgetClass)
-    {
-        NewsList->EntryWidgetClass = UWidget_NewsItemEntry::StaticClass();
-    }
-    PlayFadeIn();
+void UWidget_NewsFeed::NativeConstruct() {
+  Super::NativeConstruct();
+  if (NewsList && !NewsList->GetEntryWidgetClass()) {
+    NewsList->SetEntryWidgetClass(UWidget_NewsItemEntry::StaticClass());
+  }
+  PlayFadeIn();
 }
 
-void UWidget_NewsFeed::SetNews(const TArray<FNewsItem>& InNews)
-{
-    if (!NewsList) return;
-    TArray<UObject*> Items;
-    for (const FNewsItem& Item : InNews)
-    {
-        UNewsItemObject* Obj = NewObject<UNewsItemObject>(this);
-        Obj->Item = Item;
-        Items.Add(Obj);
-    }
-    NewsList->ClearListItems();
-    NewsList->SetListItems(Items);
+void UWidget_NewsFeed::SetNews(const TArray<FNewsItem> &InNews) {
+  if (!NewsList)
+    return;
+  TArray<UObject *> Items;
+  for (const FNewsItem &Item : InNews) {
+    UNewsItemObject *Obj = NewObject<UNewsItemObject>(this);
+    Obj->Item = Item;
+    Items.Add(Obj);
+  }
+  NewsList->ClearListItems();
+  NewsList->SetListItems(Items);
 }
 
-void UWidget_NewsFeed::PlayFadeIn()
-{
-    SetRenderOpacity(0.f);
-    float Opacity = 0.f;
-    GetWorld()->GetTimerManager().SetTimer(FadeTimer, [this, Opacity]() mutable
-    {
+void UWidget_NewsFeed::PlayFadeIn() {
+  SetRenderOpacity(0.f);
+  float Opacity = 0.f;
+  GetWorld()->GetTimerManager().SetTimer(
+      FadeTimer,
+      [this, Opacity]() mutable {
         SetRenderOpacity(Opacity);
         Opacity += 0.1f;
-        if (Opacity >= 1.f)
-        {
-            SetRenderOpacity(1.f);
-            GetWorld()->GetTimerManager().ClearTimer(FadeTimer);
+        if (Opacity >= 1.f) {
+          SetRenderOpacity(1.f);
+          GetWorld()->GetTimerManager().ClearTimer(FadeTimer);
         }
-    }, 0.05f, true);
+      },
+      0.05f, true);
 }
