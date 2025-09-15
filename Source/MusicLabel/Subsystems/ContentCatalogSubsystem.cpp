606b2c8945ddae10e1f5b274c23579ddaab43b90
johdav999 <johandavidsson@hotmail.se>
2025-09-15 21:37:23 +0200

    Generate 50s rock artists at startup

--- Source/MusicLabel/Subsystems/ContentCatalogSubsystem.cpp
+++ Source/MusicLabel/Subsystems/ContentCatalogSubsystem.cpp
@@ -1,5 +1,24 @@
 #include "ContentCatalogSubsystem.h"
+
+#include "LabelManager/Public/ArtistGenerationLibrary.h"
 #include "LabelManager/Public/LabelDataAssets.h"
+
+void UContentCatalogSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+
+    FiftiesRockArtists = UArtistGenerationLibrary::GenerateFiftiesRockArtists();
+    FiftiesRockArtistIndexByName.Reset();
+
+    for (int32 Index = 0; Index < FiftiesRockArtists.Num(); ++Index)
+    {
+        const FString& ArtistName = FiftiesRockArtists[Index].GetName();
+        if (!ArtistName.IsEmpty())
+        {
+            FiftiesRockArtistIndexByName.Add(FName(*ArtistName), Index);
+        }
+    }
+}
 
 TArray<USongAsset*> UContentCatalogSubsystem::FindSongsByGenre(FName Genre) const
 {
@@ -14,5 +33,19 @@ UArtistAsset* UContentCatalogSubsystem::GetArtistByName(FName ArtistName) const
 TArray<FString> UContentCatalogSubsystem::GetGenreTrends() const
 {
     return {};
+}
+
+bool UContentCatalogSubsystem::TryGetFiftiesRockArtistByName(FName ArtistName, FArtistAttributes& OutArtist) const
+{
+    if (const int32* ArtistIndex = FiftiesRockArtistIndexByName.Find(ArtistName))
+    {
+        if (FiftiesRockArtists.IsValidIndex(*ArtistIndex))
+        {
+            OutArtist = FiftiesRockArtists[*ArtistIndex];
+            return true;
+        }
+    }
+
+    return false;
 }
 
